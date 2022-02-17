2d2a8d6cf133a0f88571ffc576c1d91cb6abd391
Eglantine Kremer-Cochet <48824968+Etinee@users.noreply.github.com>
2022-02-17 09:05:55 +0100

    import Project

---
+++ affiche-logs.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!-- ceci est un commentaire -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="refresh" content="5" />
+    <?php require_once 'fonctions-2048.php'; ?>
+    <title></title>
+  </head>
+  <body>
+    <?php   affiche_logs(50); ?>
+  </body>
+  </html>
